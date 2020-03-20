b868786b8c2bc792b9b5d42d43edc9e5ffd2ce25
Mariusz Białecki <mariusz.bialecki@onet.pl>
2020-03-20 09:29:30 +0100

    add new functionality

--- client/app.js
+++ client/app.js
@@ -1,11 +1,16 @@
 let userName = '';
 
+const socket = io();
+
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
 const messageList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
+
+socket.on('message', ({ author, content }) => addMessage(author, content))
 
 function addMessage(author, content) {
   const message = document.createElement('li');
@@ -15,7 +20,7 @@ function addMessage(author, content) {
   message.innerHTML = `
     <h3 class="message__author">${userName === author ? 'You' : author }</h3>
     <div class="message__content">
-      ${content}
+    ${author === "Chat Bot" ? `<i>${content}</>` : content}  
     </div>
   `;
   messagesList.appendChild(message);
@@ -29,6 +34,7 @@ login = (event) => {
     
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('login', { name: userName });
   } else {
       alert('login input is empty');
     };
@@ -36,16 +42,20 @@ login = (event) => {
 
 loginForm.addEventListener('submit', login);
 
-sendMessage = (event) => {
-  event.preventDefoult();
+function sendMessage(e) {
+  e.preventDefault();
 
-  if(messageContentInput.value) {
-    addMessage(userName, messageContentInput.value)
-  } else {
-      alert('Feel in the blanks');
-  };
+  let messageContent = messageContentInput.value;
 
-  addMessageForm.reset();
-};
+  if(!messageContent.length) {
+    alert('You have to type something!');
+  }
+  else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
+    messageContentInput.value = '';
+  }
+
+}
 
 addMessageForm.addEventListener('submit', sendMessage);
